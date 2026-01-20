ac4abb19f77dd6e691c140f639b3023274fc2eb1
sonminchan <ye.son@astkorea.net>
2026-01-21 08:32:47 +0900

    init

---
+++ workplan/src/test/java/com/workplan/workplan/WorkplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workplan.workplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
